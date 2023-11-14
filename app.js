adc21a361c047261e239df6a0d7c61e1ec67b37f
voidarc <110408795+voidarclabs@users.noreply.github.com>
2023-11-14 12:41:14 +0000

    db is hard

--- app.js
+++ app.js
@@ -32,16 +32,31 @@ var con = mysql.createConnection({
     con.query("SELECT * FROM question ORDER BY question LIMIT 1 OFFSET 0;", function (err, result, fields) {
         if (err) throw err;
         console.log(result[0])
-
     })});
 
 io.on('connection', (socket) => {
     console.log(`New connection: ${socket.id}`);
+
     socket.emit('message', 'connection online big man')
+    socket.on('disconnect', function(){
+        console.log('he died lol')
+        con.query(`DELETE FROM users WHERE id LIKE '${socket.id}';`, function (err, result, fields) {
+            if (err) throw err;
+            con.destroy()
+        })
+    });
     socket.on('namecall', (data) => {
         console.log(`Name from ${socket.id}: ${data}`)
+        con.connect(function(err) {
+            if (err) throw err;
+            console.log('connected to db for insertion ;)')
+            con.query(`INSERT INTO users (${socket.id}, ${data}, 0);`, function (err, result, fields) {
+                if (err) throw err;
+                con.destroy()
+            })});
         socket.on('message', (data) => {
             console.log(`mesage from ${data.id} (${socket.id}): ${data.message}`)
+            socket.emit('message', `msg received: ${data.message}`)
         })
     })
     // socket.emit('ans', res[0])
